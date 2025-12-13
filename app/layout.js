1f3a4e046a438cba665fe8a19084d87f71469e87
qlqawy <99192875+8l8awy@users.noreply.github.com>
2025-12-13 16:41:56 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,19 @@ import { AuthProvider } from '../context/AuthContext';
 import "./globals.css";
 
 export const metadata = {
-  title: "El Agamy Materials",
-  description: "منصة المواد الدراسية",
+  title: "El Agamy Materials ",
+  description: "منصة تعليمية شاملة لطلاب العجمي، تحتوي على ملخصات، ومحاضرات محدثة باستمرار.",
+  keywords: ["اقتصاد", "كلية التجارة", "ملخصات", "العجمي", "El Agamy Materials"],
+  icons: {
+    icon: '/icon.png', // هذا السطر يربط الأيقونة التي أضفناها منذ قليل
+  },
+  openGraph: {
+    title: 'El Agamy Materials',
+    description: 'كل ما يحتاجه طالب الاقتصاد في مكان واحد.',
+    siteName: 'El Agamy Materials',
+    locale: 'ar_AR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
