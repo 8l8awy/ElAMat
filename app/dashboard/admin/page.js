955a69725ed95e133f7584ec629ba8cca6977c2f
qlqawy <99192875+8l8awy@users.noreply.github.com>
2026-01-31 20:03:08 +0200

    Update page.js

--- app/dashboard/admin/page.js
+++ app/dashboard/admin/page.js
@@ -133,7 +133,7 @@ useEffect(() => {
   }, [isAuthenticated]);
 
   const handleDelete = async (id, title) => {
-    if (adminRole !== "admin") return alert("صلاحية الحذف النهائي للمدير فقط ⛔");
+    if (adminRole !== "admin") return alert("صلاحية الحذف النهائي لقلق فقط ⛔");
     const password = prompt(`أدخل باسورد التأكيد (98612) لحذف "${title}":`);
     if (password === "98612") {
       try {
@@ -204,13 +204,13 @@ useEffect(() => {
   // شاشة الدخول (تظهر فقط عند ?mode=login وبدون كود مخزن)
   if (!isAuthenticated) {
     return (
-      <div className="min-h-screen bg-black flex items-center justify-center p-6 text-white" dir="rtl">
+      <div className="min-h-screen  flex items-center justify-center p-6 text-white" dir="rtl">
         <div className="bg-[#111] p-10 rounded-[2.5rem] border border-white/10 w-full max-w-md text-center shadow-2xl">
           <FaShieldAlt className="text-purple-500 text-5xl mx-auto mb-6" />
           <h2 className="text-xl font-black mb-6 uppercase tracking-tighter italic">Admin Access</h2>
           <input 
             type="password" placeholder="أدخل كود المشرف" 
-            className="w-full bg-black border border-white/20 p-4 rounded-2xl text-white text-center font-bold tracking-widest outline-none focus:border-purple-500 transition-all"
+            className="w-full border border-white/20 p-4 rounded-2xl text-white text-center font-bold tracking-widest outline-none focus:border-purple-500 transition-all"
             onKeyDown={(e) => e.key === 'Enter' && verifyCode(e.target.value)}
             autoFocus
           />
